b9ce21b2cf1e9fc74de669e71194d213160864fe
Imran Latif <imran.latif1503@gmail.com>
2023-11-14 20:22:41 -0500

    Removed debugging lines Fixed whitespace Removed extranious files

--- Application.py
+++ Application.py
@@ -1,9 +1,6 @@
 import mysql
 import mysql
 from mysql.connector import connection
-from database_Connect import connectDB
-
-
 def main():
     userInputList = []
     print("Answer numbers for selecting an option. ")
@@ -16,17 +13,9 @@ def main():
     print("ADO?     1.LH  2.LH  Rev  3.RH  4.RH Rev  5.Maglock  6.Electric Crashbar  7.Not Opening  8.None ")
     userInputList.append(input(">> "))
 
-    print("DEBUG USERINPUTLIST: "+ str(userInputList))
-
-    # checkDB(userInputList)
-
     # Create the connection object    IF ERROR ADD PASSWORD
     myconn = mysql.connector.connect(host='localhost', database='DoorHardwareVV', user='root', password='')
 
-    # printing the connection object
-    # print(myconn)
-
-    # creating the cursor object
     cursor = myconn.cursor()
 
     cursor.execute(
@@ -34,17 +23,15 @@ def main():
         % (int(userInputList[0]), int(userInputList[1]), int(userInputList[2]), int(userInputList[3]))
     )
     result = cursor.fetchone()
-    print("DEBUG result: " + str(result))
 
     if str(result) == "(0,)":
         print("No compatability.")
         print("Please Try Again. >>\n\n")
         main()
+
     else:
         print("Database says these components should work together.")
-
         exit(0)
-
 
 if __name__ == '__main__':
     main()
